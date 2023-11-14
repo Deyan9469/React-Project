8cb14590ae5c75441f05d49d23f07fae93f7542e
Deyan9469 <Deyan9469@users.noreply.github.com>
2023-11-14 17:34:24 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Navigation from "./components/Navigation"
+import './styles_template.css'
+function App() {
+
+
+  return (
+    <>
+      <Navigation />
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default App
